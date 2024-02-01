9f37b6dc256da489f35f0190102ba7f7786f0a45
Andre Grillo <andrell.grillo@gmail.com>
2024-02-01 16:41:10 -0300

    First - Criando Senha com Has

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common'
+import { hash } from 'bcryptjs'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Controller('/accounts')
+export class CreateAccountnController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(@Body() body: any) {
+    const { name, email, password } = body
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+    if (userWithSameEmail) {
+      throw new ConflictException('User with same e-mail addres already exist.')
+    }
+
+    const hashPassword = await hash(password, 8)
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashPassword,
+      },
+    })
+  }
+}
