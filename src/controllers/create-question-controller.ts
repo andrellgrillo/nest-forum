4eb7618a109741378f1f9481c3d62135a1537273
Andre Grillo <andrell.grillo@gmail.com>
2024-03-05 13:44:28 -0300

    Add JWT Strategy & AuthGuard

--- src/controllers/create-question-controller.ts
+++ src/controllers/create-question-controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Post } from '@nestjs/common'
+import { Controller, Post, UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { PrismaService } from 'src/prisma/prisma.service'
 
 @Controller('/questions')
+@UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
   constructor(private prisma: PrismaService) {}
 
