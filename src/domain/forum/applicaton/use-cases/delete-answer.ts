6836fbdb54e2cf8ebf4d13f9ef4cb304bc4d26ea
Andre Luiz Leite Grillo <andrell.grillo@gmail.com>
2024-04-22 10:09:58 -0300

    delete answer - e2e

--- src/domain/forum/applicaton/use-cases/delete-answer.ts
+++ src/domain/forum/applicaton/use-cases/delete-answer.ts
@@ -2,6 +2,7 @@ import { Either, left, right } from '@/core/either'
 import { AnswersRepository } from '../repositories/answers-repository'
 import { ResourceNotFoundError } from '../../../../core/errors/errors/resource-not-found'
 import { NotAllowedError } from '../../../../core/errors/errors/not-allowed-error'
+import { Injectable } from '@nestjs/common'
 
 interface DeleteAnswerUseCaseRequest {
   authorId: string
@@ -15,6 +16,7 @@ type DeleteAnswerUseCaseResponse = Either<
   {}
 >
 
+@Injectable()
 export class DeleteAnswerUseCase {
   constructor(private answersRepository: AnswersRepository) {}
 
