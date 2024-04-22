9f389ccc5806407b011bb7634dc2d91897d4dc70
Andre Luiz Leite Grillo <andrell.grillo@gmail.com>
2024-04-22 15:46:56 -0300

    fetch question comment - e2e

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -33,6 +33,8 @@ import { CommentOnAnswerController } from './controllers/comment-on-answer.contr
 import { CommentOnAnswerUseCase } from '@/domain/forum/applicaton/use-cases/comment-on-answer'
 import { DeleteAnswerCommentController } from './controllers/delete-answer-comment-controller'
 import { DeleteAnswerCommentUseCase } from '@/domain/forum/applicaton/use-cases/delete-comment-answer'
+import { FetchQuestionCommentsController } from './controllers/fetch-question-comments.controller'
+import { FetchQuestionCommentsUseCase } from '@/domain/forum/applicaton/use-cases/fetch-question-comments'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -53,6 +55,7 @@ import { DeleteAnswerCommentUseCase } from '@/domain/forum/applicaton/use-cases/
     DeleteQuestionCommentController,
     CommentOnAnswerController,
     DeleteAnswerCommentController,
+    FetchQuestionCommentsController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -71,6 +74,7 @@ import { DeleteAnswerCommentUseCase } from '@/domain/forum/applicaton/use-cases/
     DeleteQuestionCommentUseCase,
     CommentOnAnswerUseCase,
     DeleteAnswerCommentUseCase,
+    FetchQuestionCommentsUseCase,
   ],
 })
 export class HttpModule {}
